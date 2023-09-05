6eff135570e2ef5028ee15c59fc68bb175fdf66c
WVGarrison <will.v.garrison@gmail.com>
2023-09-05 15:56:36 -0500

    Fixed king moving

--- ai.cpp
+++ ai.cpp
@@ -9,7 +9,7 @@
 using namespace std;
 
 Move BESTMOVE = *new Move();
-int BESTVAL = INT_MIN;
+float BESTVAL = INT_MIN;
 bool ISWHITE;
 int DEPTH = 0;
 int CUTTOFFS = 0;
@@ -17,19 +17,19 @@ int NODES;
 
 int PieceVals[] = {0, 1, 3, 3, 5, 7, 100};
 
-int eval(Board board);
+float eval(Board board);
+float eval(Board board , Move move);
 
-int eval(Board board, Move move);
 
 void sortMove(Board board, list<Move> moves) {
-    moves.sort([](const Move &a, const Move &b) {
-        return a.isCapture < b.isCapture;
-    });
 
+    moves.sort([&board](const Move &a, const Move &b) {
+        return eval(board,board.getMove(a.from, a.to)) < eval(board,board.getMove(b.from, b.to));
+    });
 }
 
-int quiessence(Board board, int alpha, int beta) {
-    int standPat = eval(board);
+float quiessence(Board board, float alpha, float beta) {
+    float standPat = eval(board);
     if (standPat >= beta) {
         return beta;
     }
@@ -41,7 +41,7 @@ int quiessence(Board board, int alpha, int beta) {
     for (auto x: legalMoves) {
         if (x.isCapture) {
             board.makeMove(x);
-            int score = -quiessence(board, -beta, -alpha);
+            float score = -quiessence(board, -beta, -alpha);
             board.undoMove();
             if (score >= beta) {
                 return beta;
@@ -54,11 +54,11 @@ int quiessence(Board board, int alpha, int beta) {
     return alpha;
 }
 
-int minMax(Board board, Move thisMove, int depth, int ply, int alpha, int beta) {
+float minMax(Board board, Move thisMove, int depth, int ply, float alpha, float beta) {
     NODES++;
     if (depth == 0) {
 
-        int value = eval(board);
+        float value = quiessence(board, -beta, -alpha);
 
         return value;
     }
@@ -67,7 +67,7 @@ int minMax(Board board, Move thisMove, int depth, int ply, int alpha, int beta) 
     sortMove(board, legalMoves);
     for (auto x: legalMoves) {
         board.makeMove(x);
-        int value = -minMax(board, x, depth - 1, ply + 1, -beta, -alpha);
+        float value = -minMax(board, x, depth - 1, ply + 1, -beta, -alpha);
 
         board.undoMove();
         if (value >= beta) {
@@ -79,13 +79,11 @@ int minMax(Board board, Move thisMove, int depth, int ply, int alpha, int beta) 
             alpha = value;
             if (ply == 0) {
                 BESTMOVE = x;
-                if (ply == 0) {
                     char x1 = Piece::getFile(x.from) + 'a';
                     char y1 = Piece::getRank(x.from) + '1';
                     char x2 = Piece::getFile(x.to) + 'a';
                     char y2 = Piece::getRank(x.to) + '1';
                     cout << "Bestmove: " << x1 << y1 << x2 << y2 << endl;
-                }
             }
 
         }
@@ -97,13 +95,15 @@ int minMax(Board board, Move thisMove, int depth, int ply, int alpha, int beta) 
 
 }
 
-int eval(Board board) {
+
+
+float eval(Board board) {
     // sum of all pieces
     float boardVal = 0;
     list<Move> legalMoves = board.generateLegalMoves();
     // Detect checkmate
 
-    if (legalMoves.size() == 0) {
+    if (legalMoves.empty()) {
         if (board.isWhiteMove) {
             return INT_MIN + 1 * (ISWHITE ? 1 : -1);
         } else {
@@ -115,22 +115,48 @@ int eval(Board board) {
         int x = board.squares[i];
         bool isWhite = Piece::getSide(x) == Piece::White;
         int type = Piece::getPieceType(x);
+        int rank = Piece::getRank(i);
+        int file = Piece::getFile(i);
+        // reward for moving pawns up the board
+        if (type == Piece::Pawn) {
+
+            if (isWhite) {
+                boardVal += rank * 0.1f;
+            } else {
+                boardVal -= (7 - rank) * 0.1f;
+            }
+
+
+
+
+        }
+        // multiplier for pieces in the center 4 files of the board
+        if (file > 2 && file < 6) {
+            if (isWhite) {
+                boardVal += 0.1f;
+            } else {
+                boardVal -= 0.1f;
+            }
+        }
+
         int sideMulti = isWhite == board.isWhiteMove ? 1 : -1;
         boardVal += PieceVals[type] * sideMulti;
+
 
     }
     //reward for having more legal moves than opponent
 
-    float legalMovesVal = legalMoves.size() * 0.03;
+    float legalMovesVal = legalMoves.size() * 0.03f;
 
 
     return boardVal + legalMovesVal;
 
 }
 
-int eval(Move move, Board board) {
+float eval(Board board, Move move){
+    // Evaluate a move
     board.makeMove(move);
-    int value = eval(board);
+    float value = eval(board);
     board.undoMove();
     return value;
 }
@@ -140,7 +166,7 @@ Move findBestMove(Board board) {
     ISWHITE = board.isWhiteMove;
     clock_t start = clock();
     DEPTH = 0;
-    while (((clock() - start) * 1000) / CLOCKS_PER_SEC < 600000) {
+    while (((clock() - start) * 1000) / CLOCKS_PER_SEC < 6000) {
         NODES = 0;
         CUTTOFFS = 0;
         DEPTH++;
